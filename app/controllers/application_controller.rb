8419c02e1425c931c44c23af857212d98c95fb1c
Elle Harding <ehardin0@chicagobooth.edu>
2023-04-06 15:37:30 +0000

    Final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -10,9 +10,15 @@ class ApplicationController < ActionController::Base
   end
 
   def play_rock
-    # write a ton of code
+    @comp_move = ["rock", "paper", "scissors"].sample
 
-    # redirect_to("https://www.wikipedia.org")
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
 
     render({ :template => "game_templates/user_rock.html.erb"})
   end
@@ -32,6 +38,16 @@ class ApplicationController < ActionController::Base
   end
 
   def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
     render({ :template => "game_templates/user_scissors.html.erb"})
   end
 end
